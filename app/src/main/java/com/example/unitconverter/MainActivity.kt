e76c6ff29ae94a79808142185a8c0f12597773a3
Space-Belt <dngurwn@gmail.com>
2025-04-10 22:28:24 +0900

    feat: 기본 단위변환기 완성

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -24,12 +24,18 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +57,22 @@ fun ReusableSpacer(cusotomHeight: Int) {
 
 @Composable
 fun UnitConverter() {
+
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("m") }
+    var outputUnit by remember { mutableStateOf("m") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val conversionFactor = remember { mutableStateOf(1.00) }
+    val oConversionFactor = remember { mutableStateOf(1.00) }
+
+    fun convertUnits() {
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0
+        val result = (inputValueDouble * conversionFactor.value * 100.0 / oConversionFactor.value).roundToInt() / 100.0
+        outputValue = result.toString()
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -58,40 +80,88 @@ fun UnitConverter() {
     ) {
         Text("Unit Converter")
 
-        ReusableSpacer(100)
+        ReusableSpacer(20)
         OutlinedTextField(
-            modifier = Modifier.padding(),
-            value = "",
-            onValueChange = {},
+            modifier = Modifier.padding(bottom = 20.dp  ),
+            value = inputValue,
+            onValueChange = {
+                inputValue = it
+                convertUnits()
+            },
+            label = { (Text("입력해주세요")) }
             )
         Row {
             Box {
-                Button(onClick = { }) {
-                    Text("선택")
+                Button(onClick = {
+                    iExpanded = !iExpanded
+                }) {
+                    Text(text = inputUnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "아래 화살표" )
                 }
-                DropdownMenu(expanded = false, onDismissRequest = {/* 드롭다운 실행시 */}) {
-                    DropdownMenuItem(text = {Text("cm")}, onClick = {})
-                    DropdownMenuItem(text = {Text("m")}, onClick = {})
-                    DropdownMenuItem(text = {Text("mm")}, onClick = {})
-                    DropdownMenuItem(text = {Text("CM")}, onClick = {})
+                DropdownMenu(expanded = iExpanded, onDismissRequest = {iExpanded = !iExpanded}) {
+                    DropdownMenuItem(text = {Text("cm")}, onClick = {
+                        iExpanded = !iExpanded
+                        inputUnit = "cm"
+                        conversionFactor.value = 0.01
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {Text("m")}, onClick = {
+                        iExpanded = !iExpanded
+                        inputUnit = "m"
+                        conversionFactor.value = 1.0
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {Text("mm")}, onClick = {
+                        iExpanded = !iExpanded
+                        inputUnit = "mm"
+                        conversionFactor.value = 0.001
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {Text("km")}, onClick = {
+                        iExpanded = !iExpanded
+                        inputUnit = "km"
+                        conversionFactor.value = 1000.0
+                        convertUnits()
+                    })
                 }
             }
             Spacer(modifier = Modifier.width(16.dp))
             Box {
-                Button(onClick = { }) {
-                    Text("선택")
+                Button(onClick = {
+                    oExpanded = !oExpanded
+                }) {
+                    Text(text = outputUnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "아래 화살표" )
                 }
-                DropdownMenu(expanded = true, onDismissRequest = {/* 드롭다운 실행시 */}) {
-                    DropdownMenuItem(text = {Text("cm")}, onClick = {})
-                    DropdownMenuItem(text = {Text("m")}, onClick = {})
-                    DropdownMenuItem(text = {Text("mm")}, onClick = {})
-                    DropdownMenuItem(text = {Text("CM")}, onClick = {})
+                DropdownMenu(expanded = oExpanded, onDismissRequest = { oExpanded = !oExpanded }) {
+                    DropdownMenuItem(text = {Text("cm")}, onClick = {
+                        oExpanded = !oExpanded
+                        outputUnit = "cm"
+                        oConversionFactor.value = 0.01
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {Text("m")}, onClick = {
+                        oExpanded = !oExpanded
+                        outputUnit = "m"
+                        oConversionFactor.value = 1.00
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {Text("mm")}, onClick = {
+                        oExpanded = !oExpanded
+                        outputUnit = "mm"
+                        oConversionFactor.value = 0.001
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {Text("km")}, onClick = {
+                        oExpanded = !oExpanded
+                        outputUnit = "km"
+                        oConversionFactor.value = 1000.0
+                        convertUnits()
+                    })
                 }
             }
         }
-        Text("Result:")
+        Text("Result: $outputValue $outputUnit")
     }
 }
 
