543488c5ee40014bb330383db72475f0958ed0b9
Space-Belt <dngurwn@gmail.com>
2025-04-10 13:03:57 +0900

    feat: Modifier, layout정렬, Spacer, padding 학습

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -5,10 +5,13 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -19,9 +22,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,21 +42,26 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun ReusableSpacer(cusotomHeight: Int) {
+    Spacer(modifier = Modifier.height(cusotomHeight.dp))
+}
 
 @Composable
 fun UnitConverter() {
-    Column {
-        Text("Unit Converter")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Unit Converter", modifier = Modifier.padding(horizontal = 100.dp, vertical = 100.dp))
+
+        ReusableSpacer(1000)
         OutlinedTextField(
+            modifier = Modifier.padding(),
             value = "",
-            onValueChange = {
-              //String 받아서 -> Unit
-            },
+            onValueChange = {},
             )
-        DropdownMenuItem(
-            onClick = {},
-            text = {Text("아하아")},
-        )
         Row {
             Box {
                 Button(onClick = { }) {
